c0ed2d061404bfa99374229498ae9418094a9bf5
weissercn <weissercn@gmail.com>
2016-06-30 19:40:15 -0400

    set up pip install

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='learningml',
+      version='0.1',
+      description='This repository demonstrates how to make a project pip installable, write a Python module in C++ and use scikit-learn, keras and spearmint.',
+      url='https://github.com/weissercn/learningml',
+      author='Constantin Weisser',
+      author_email='weissercn@gmail.com',
+      license='MIT',
+      packages=['learningml'],
+      zip_safe=False)
