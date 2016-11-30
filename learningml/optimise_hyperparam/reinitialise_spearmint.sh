a886198e6bd35a74734e8824a66fbb033a89a58f
weissercn <weissercn@gmail.com>
2016-11-29 21:35:40 -0500

    removed optimise_hyperparameters, because I started a new repository called spearmint_wrapper

--- learningml/optimise_hyperparam/reinitialise_spearmint.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash
-
-PROCESS="$(pgrep mongo)"
-if [ -z "$PROCESS" ];then
-	echo "No mongo process is running"
-else
-	echo "Mongo process $PROCESS is running, killing this process..."
-	kill $PROCESS
-fi
